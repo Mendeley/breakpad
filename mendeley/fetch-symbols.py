8e6594e91ec0693f441f80370d9c14ba06589c1a
Robert Knight <robert.knight@mendeley.com>
2014-11-27 18:29:25 +0000

    Correct logic for constructing symbol file names
    The path to the symbol file name is:
      $DEBUG_FILE_NAME/$BUILD_ID/$SYMFILE_NAME
    
    Where $DEBUG_FILE_NAME is the name of the file from
    which the debug info was originally extracted - the PDB
    on Windows or the executable / shared library directly
    on other platforms.
    
    $SYMFILE_NAME is the same as $DEBUG_FILE_NAME minus the '.pdb'
    suffix, if it has one.

--- mendeley/fetch-symbols.py
+++ mendeley/fetch-symbols.py
@@ -45,22 +45,26 @@ MAX_MISSING_CACHE_AGE = 300
 
 def main():
     parser = argparse.ArgumentParser(
-"""Fetch symbol files for a binary from Mendeley Desktop's symbol server and cache them locally.
+"""Fetch symbol files for a binary from an HTTP symbol server and cache them locally.
 """)
     parser.add_argument('-s', type=str,
       action='append',
       dest='symbol_servers',
       help='Add a symbol server to search for available symbols',
       required=True)
-    parser.add_argument('binary_name', type=str, help='The file name (excluding the path) of the executable or shared library', action='store')
+    parser.add_argument('debug_file_name', type=str, help='The file name (excluding the path) of the file (PDB on Windows, shared library or executable on other platforms) which the debug info was extracted from.', action='store')
     parser.add_argument('debug_id', type=str, help='The debug/build identifier for the version of the binary referenced in a minidump', action='store')
     opts = parser.parse_args()
 
-    binary_name = opts.binary_name
+    debug_file_name = opts.debug_file_name
     debug_id = opts.debug_id
-    binary_basename, binary_ext = os.path.splitext(binary_name)
 
-    rel_path = '%s/%s/%s.sym' % (binary_name, debug_id, binary_basename)
+    if debug_file_name.endswith('.pdb'):
+        symfile_name = debug_file_name[0:-4] + '.sym'
+    else:
+        symfile_name = debug_file_name + '.sym'
+
+    rel_path = '%s/%s/%s' % (debug_file_name, debug_id, symfile_name)
     cache_path = '%s/%s' % (CACHE_ROOT, rel_path)
     missing_entry_cache_path = '%s.failed' % cache_path
 
@@ -76,10 +80,10 @@ def main():
         if os.path.isfile(missing_entry_cache_path):
             cache_age = time.time() - os.path.getmtime(missing_entry_cache_path)
             if cache_age < MAX_MISSING_CACHE_AGE:
-               print('Symbols for %s not found in cache but failed lookup cached %d seconds ago' % (binary_name, cache_age), file=sys.stderr)
+               print('Symbols for %s not found in cache but failed lookup cached %d seconds ago' % (debug_file_name, cache_age), file=sys.stderr)
                continue
 
-        print('Symbols for %s not found in cache, fetching from %s' % (binary_name, symbol_url), file=sys.stderr)
+        print('Symbols for %s not found in cache, fetching from %s' % (debug_file_name, symbol_url), file=sys.stderr)
         
         mkpath(os.path.dirname(cache_path))
 
@@ -100,7 +104,7 @@ def main():
                 os.remove(missing_entry_cache_path)
 
         except urllib2.HTTPError as err:
-            print('Symbols not found for %s: %s' % (binary_name, err), file=sys.stderr)
+            print('Symbols not found for %s: %s' % (debug_file_name, err), file=sys.stderr)
             print('Caching miss in %s' % (missing_entry_cache_path), file=sys.stderr)
 
             symbols_missing_file = open(missing_entry_cache_path, 'w')
